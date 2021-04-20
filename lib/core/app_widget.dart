645e8f8c73fc6166d3e64463c861452d9b5dcd79
Andre Peixoto <peixoto.amp@gmail.com>
2021-04-20 16:40:30 -0300

    Updates core variables for Styling

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,11 +1,19 @@
 import 'package:DevQuiz/home/home_page.dart';
 // import 'package:DevQuiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.light,
+      ),
+    );
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'DevQuiz',
       home: HomePage(),
     );

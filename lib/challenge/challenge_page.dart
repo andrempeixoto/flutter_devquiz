5b56b8dcc65dddf4d878c70dd744fd5b01906a7e
Andre Peixoto <peixoto.amp@gmail.com>
2021-04-21 03:24:03 -0300

    Creates ChallengePage and QuestionIndicatorWidget

---
+++ lib/challenge/challenge_page.dart
@@ -0,0 +1,33 @@
+import 'package:DevQuiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
+import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class ChallengePage extends StatefulWidget {
+  @override
+  _ChallengePageState createState() => _ChallengePageState();
+}
+
+class _ChallengePageState extends State<ChallengePage> {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarIconBrightness: Brightness.dark,
+      ),
+    );
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(80),
+        child: SafeArea(
+          top: true,
+          child: QuestionIndicatorWidget(),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: QuizWidget(title: "Titulo Quiz Widget"),
+      ),
+    );
+  }
+}

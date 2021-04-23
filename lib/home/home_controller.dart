f387b870e9f092659c424ce6e5cf58ef9f1d9c2c
Andre Peixoto <peixoto.amp@gmail.com>
2021-04-23 00:25:28 -0300

    Creates Database and adds JSON dynamic rendering

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -1,7 +1,5 @@
-import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/home/home_repository.dart';
 import 'package:DevQuiz/home/home_state.dart';
-import 'package:DevQuiz/shared/models/answer_model.dart';
-import 'package:DevQuiz/shared/models/question_model.dart';
 import 'package:DevQuiz/shared/models/quiz_model.dart';
 import 'package:DevQuiz/shared/models/user_model.dart';
 import 'package:flutter/foundation.dart';
@@ -14,50 +12,17 @@ class HomeController {
   UserModel? user;
   List<QuizModel>? quizzes;
 
+  final repository = HomeRepository();
+
   void getUser() async {
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
-
-    user = UserModel(
-      name: "Andre",
-      photoUrl: 'https://avatars.githubusercontent.com/u/35813862?v=4',
-    );
+    user = await repository.getUser();
     state = HomeState.success;
   }
 
   void getQuizzes() async {
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
-
-    quizzes = [
-      QuizModel(
-        title: "NLW 5 Flutter",
-        questions: [
-          QuestionModel(title: "Está curtindo o Flutter?", answers: [
-            AnswerModel(title: "Estou curtindo"),
-            AnswerModel(title: "Estou amando"),
-            AnswerModel(title: "Show de bola"),
-            AnswerModel(title: "Muito top", isRight: true),
-          ])
-        ],
-        image: AppImages.blocks,
-        level: Level.facil,
-      ),
-      QuizModel(
-        title: "NLW 6 Flutter",
-        questions: [
-          QuestionModel(title: "Está curtindo o Flutter?", answers: [
-            AnswerModel(title: "Estou curtindo"),
-            AnswerModel(title: "Estou amando"),
-            AnswerModel(title: "Show de bola"),
-            AnswerModel(title: "Muito top", isRight: true),
-          ])
-        ],
-        image: AppImages.blocks,
-        level: Level.facil,
-      )
-    ];
-
+    quizzes = await repository.getQuizzes();
     state = HomeState.success;
   }
 }
--- lib/shared/models/quiz_model.dart
+++ lib/shared/models/quiz_model.dart
@@ -1,19 +1,63 @@
+import 'dart:convert';
+
 import 'package:DevQuiz/shared/models/question_model.dart';
 
 enum Level { facil, medio, dificil, perito }
 
+extension LevelExtString on String {
+  static Level fromMap(String key) => {
+        "facil": Level.facil,
+        "medio": Level.medio,
+        "dificil": Level.dificil,
+        "perito": Level.perito,
+      }[key]!;
+}
+
+extension LevelExt on Level {
+  static String toMap(String key) => {
+        Level.facil: "facil",
+        Level.medio: "medio",
+        Level.dificil: "dificil",
+        Level.perito: "perito"
+      }[key]!;
+}
+
 class QuizModel {
   final String title;
-  final List<QuestionModel> questions;
-  final int questionsAnswered;
   final String image;
   final Level level;
+  final List<QuestionModel> questions;
+  final int questionsAnswered;
 
   QuizModel({
     required this.title,
-    required this.questions,
-    this.questionsAnswered = 0,
     required this.image,
     required this.level,
+    required this.questions,
+    this.questionsAnswered = 0,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'image': image,
+      'level': LevelExt.toMap,
+      'questions': questions.map((x) => x.toMap()).toList(),
+      'questionsAnswered': questionsAnswered,
+    };
+  }
+
+  factory QuizModel.fromMap(Map<String, dynamic> map) {
+    return QuizModel(
+      title: map['title'],
+      image: map['image'],
+      level: LevelExtString.fromMap(map['level']),
+      questions: List<QuestionModel>.from(map['questions']?.map((x) => QuestionModel.fromMap(x))),
+      questionsAnswered: map['questionsAnswered'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory QuizModel.fromJson(String source) => QuizModel.fromMap(json.decode(source));
 }
